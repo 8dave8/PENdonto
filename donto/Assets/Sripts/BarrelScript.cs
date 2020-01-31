ee6ad97f002dc992f3dd9ee397c777935a841083
8dave8 <43006387+8dave8@users.noreply.github.com>
2020-01-31 10:40:05 +0100

    asd

---
+++ donto/Assets/Sripts/BarrelScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelScript : MonoBehaviour
+{
+    public GameObject particles;
+    public GameObject[] Coins;
+    private static System.Random rng;
+    void Start()
+    {
+        rng = new System.Random();
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "sword") death();
+    }
+    public void death(){         
+        Instantiate(particles,transform.position,Quaternion.identity);
+        for (int i = 0; i <= 2; i++)
+        {
+            int rnd = rng.Next(0,2);
+            GameObject coin = Instantiate(Coins[rnd],new Vector3(transform.position.x,transform.position.y+1,0), Quaternion.identity);
+            coin.GetComponent<Rigidbody2D>().velocity = new Vector2(rng.Next(2, 4),rng.Next(3, 6));
+        }    
+        Destroy(this.gameObject); 
+    }
+}
---
+++ donto/Assets/Sripts/DestroyOverTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOverTime : MonoBehaviour
+{
+    public float Lifetime;
+    private float start = 0;
+    void Update()
+    {
+        start += Time.deltaTime;
+        if (start >= Lifetime)  Destroy(gameObject);
+    }
+}
